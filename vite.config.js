ac03a107643d39f8685b53dd8a4d3ad76d2f8845
GANSOONLEE <vincentgan0402@gmail.com>
2024-08-12 02:22:30 +0800

    fix. vite.config.js base 設定

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const mode = 'production';
+
 export default defineConfig({
   plugins: [vue()],
   build: {
     outDir: 'docs',
   },
+  base: mode === 'production' ? '/assets/' : './assets/',
 })
